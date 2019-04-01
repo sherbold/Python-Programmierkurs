ae4872c4f3183aeed0638bb195a6c9c6fe94724d
sherbold <herbold@cs.uni-goettingen.de>
2019-04-01 11:20:54 +0200

    example files for packaging

---
+++ examples/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="course-sample-package",
+    version="0.0.1",
+    author="Steffen Herbold",
+    author_email="herbold@cs.uni-goettingen.de",
+    description="A small example package for a programming course",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sherbold/Python-Programmierkurs/tree/master/examples",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
